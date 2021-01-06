1b39927aeba60ed0fdf7e890857a5abc28a9bde7
Jan Sierakowski <jasiekxl@gmail.com>
2021-01-06 18:56:33 +0100

    OOP code transformation

---
+++ CarTryApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using CarTry.App.Abstract;
+using CarTry.App.Concrete;
+using CarTry.App.Manager;
+using CarTry.Domain.Entity;
+using System;
+
+namespace CarTryApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuActionService actionService = new MenuActionService();
+            ItemManger itemManger = new ItemManger(actionService);
+
+            Console.WriteLine("Welcome in first car test&try community");
+            Console.WriteLine("Select what you want to do:");
+
+            while (true)
+            {
+                var mainMenu = actionService.GetMenuActionsByName("Main");
+                for (int i = 0; i < mainMenu.Count; i++)
+                {
+                    Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+                }
+                var operation = Console.ReadKey();
+
+                Console.WriteLine();
+
+                switch (operation.KeyChar)
+                {
+                    case '1':
+                        var newId = itemManger.AddNewItem();
+                        break;
+                    case '2':
+                        var removeId = itemManger.RemoveItem();
+                        Console.WriteLine();
+                        break;
+                    case '3':
+                        var detailId = itemManger.ItemDetail();
+                        Console.WriteLine();
+                        break;
+                    case '4':
+                        var brandToShow = itemManger.CarBrandsByType();
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Action you entered does not exist");
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Szkoladotneta-kurs
